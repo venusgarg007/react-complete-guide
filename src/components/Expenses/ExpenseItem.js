63e4af482bffdd1f537237f15a771701e58cf4c0
Venus Garg <garg.venus39@gmail.com>
2022-02-25 12:27:20 +0530

    working with states - useState hook

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,25 @@
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  
+  const [title, setTitle] = useState(props.title);
+
+  // useState returns an array where [0] returns the value itself in the argument and [1] returns the function to update the initial value.
+  // Hence using array destructuring to store value in title and point the setTitle to the function that can update the initial value.
+
   const clickHandler = () => {
+    setTitle("Updated!");
     console.log("Clicked!!!");
   };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
